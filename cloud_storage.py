8c8e4351c340dbe579df0ca0c711aff216b7a76b
cocheae <cocheae@umich.edu>
2020-07-15 22:25:27 -0400

    addded notes

--- cloud_storage.py
+++ cloud_storage.py
@@ -11,21 +11,24 @@ for i in range(len(name_of_majors)):
     db_cursor.put('/Social_Studies_open_major_url', name_of_majors[i], url_with_text[i])
 
 db_cursor.put('/', 'Major_and_Reqs', None)
-# db_cursor.put('/Major_and_Reqs', requirements_soup.h1.text.strip(), requirements_soup.select("div.reqs"))
 
 
-major_n_req = {}
+# major_n_req = {}
 for i in url_tails:
-    driver.get('https://lsa.umich.edu{}'.format(i))
-    requirements_soup = BeautifulSoup(driver.page_source, "html.parser")
-    target = requirements_soup.select('div.reqs ul li')
-    names = [n.text for n in target]
+    driver.get('https://lsa.umich.edu{}'.format(i))      # visits page of major
+    requirements_soup = BeautifulSoup(driver.page_source, "html.parser")  #creates new soup
+    target = requirements_soup.select('div.reqs ul li')  #Only looks for the classes (ideally, this is broken)
+    names = [n.text for n in target]  #strips the text from the li tags
     key = ''
     value = ''
-    keys = '/Major_and_Reqs/{}'.format(requirements_soup.h1.text.strip())
-    db_cursor.put('/', keys, None)
+
+    keys = '/Major_and_Reqs/{}'.format(requirements_soup.h1.text.strip())  #updates according to the new major
+    db_cursor.put('/', keys, None)  #creates new level for each major
+    #logs into the database the classes required for the major
     for j in range(len(names)):
+        #splits the li element into the class number and the description
         names[j] = names[j].split(':')[0]
+        #splits the class again into subject and number
         key = names[j].split()[0]
         value = names[j].split()[1]
         db_cursor.put(keys, key, value)
