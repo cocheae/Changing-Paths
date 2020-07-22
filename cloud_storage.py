f7364bdeacbe71253503b69561c347ad4a0df296
cocheae <cocheae@umich.edu>
2020-07-22 00:36:40 -0400

    update

--- cloud_storage.py
+++ cloud_storage.py
@@ -5,35 +5,34 @@ from just_scraping import *
 
 db_cursor = firebase.FirebaseApplication('https://web-scrapping-de6ca.firebaseio.com/', None)
 
-db_cursor.put('/', 'Social_Studies_open_major_url', None)
+# db_cursor.put('/', 'open_major_url', None)
 
-for i in range(len(name_of_majors)):
-    db_cursor.put('/Social_Studies_open_major_url', name_of_majors[i], url_with_text[i])
+# for i in range(len(name_of_majors)):
+#     db_cursor.put('/open_major_url', name_of_majors[i].replace('[', '(').replace(']', ')').replace('.', '').replace('/', '_'), url_with_text[i])
+#     # if name_of_majors[i] == 'Biochemistry [B.S.] (Major)':
+#     #     db_cursor.put('/open_major_url', 'Biochemistry (B.S.) (Major)', url_with_text[i])
+#     # else:
+#     #     db_cursor.put('/open_major_url', name_of_majors[i], url_with_text[i])
 
 db_cursor.put('/', 'Major_and_Reqs', None)
 
 
-# major_n_req = {}
-for i in url_tails:
-    driver.get('https://lsa.umich.edu{}'.format(i))      # visits page of major
+for i in range(len(url_tails)):
+    driver.get('https://lsa.umich.edu{}'.format(url_tails[i]))      # visits page of major
     requirements_soup = BeautifulSoup(driver.page_source, "html.parser")  #creates new soup
-    target = requirements_soup.select('div.reqs ul li')  #Only looks for the classes (ideally, this is broken)
+    target = requirements_soup.select('div.current-program-requirements div.reqs ul li')  #Only looks for the classes (ideally, this is broken)
     names = [n.text for n in target]  #strips the text from the li tags
-    key = ''
-    value = ''
-
-    keys = '/Major_and_Reqs/{}'.format(requirements_soup.h1.text.strip())  #updates according to the new major
-    db_cursor.put('/', keys, None)  #creates new level for each major
-    #logs into the database the classes required for the major
+    major = requirements_soup.h1.text.strip().replace('[', '(').replace(']', ')').replace('.', '').replace('/', '_')
+    major_checker = '/Major_and_Reqs/{}'.format(major)  #updates according to the new major
+    # print(major)
+    if not names or major == 'History Major':
+        db_cursor.post(major_checker, 'do manually')
+        continue
     for j in range(len(names)):
         #splits the li element into the class number and the description
-        names[j] = names[j].split(':')[0]
-        #splits the class again into subject and number
-        key = names[j].split()[0]
-        value = names[j].split()[1]
-        db_cursor.put(keys, key, value)
-
-
-
-
-    # time.sleep(5)
+        if major == 'Economics Major':
+            names[j] = names[j].split(',')[0]
+        else:
+            names[j] = names[j].split(':')[0]
+        db_cursor.post(major_checker, names[j])
+    time.sleep(5)
