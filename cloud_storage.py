6ccfe52d0f1a3e400428152ddca1466ede63cf9f
cocheae <cocheae@umich.edu>
2020-07-22 01:26:24 -0400

    database finished

--- cloud_storage.py
+++ cloud_storage.py
@@ -1,38 +1,22 @@
-import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from firebase import firebase
-from selenium import webdriver
-from just_scraping import *
 
-db_cursor = firebase.FirebaseApplication('https://web-scrapping-de6ca.firebaseio.com/', None)
-
-# db_cursor.put('/', 'open_major_url', None)
-
-# for i in range(len(name_of_majors)):
-#     db_cursor.put('/open_major_url', name_of_majors[i].replace('[', '(').replace(']', ')').replace('.', '').replace('/', '_'), url_with_text[i])
-#     # if name_of_majors[i] == 'Biochemistry [B.S.] (Major)':
-#     #     db_cursor.put('/open_major_url', 'Biochemistry (B.S.) (Major)', url_with_text[i])
-#     # else:
-#     #     db_cursor.put('/open_major_url', name_of_majors[i], url_with_text[i])
-
-db_cursor.put('/', 'Major_and_Reqs', None)
+db = firebase.FirebaseApplication('https://web-scrapping-de6ca.firebaseio.com/')
 
 
-for i in range(len(url_tails)):
-    driver.get('https://lsa.umich.edu{}'.format(url_tails[i]))      # visits page of major
-    requirements_soup = BeautifulSoup(driver.page_source, "html.parser")  #creates new soup
-    target = requirements_soup.select('div.current-program-requirements div.reqs ul li')  #Only looks for the classes (ideally, this is broken)
-    names = [n.text for n in target]  #strips the text from the li tags
-    major = requirements_soup.h1.text.strip().replace('[', '(').replace(']', ')').replace('.', '').replace('/', '_')
-    major_checker = '/Major_and_Reqs/{}'.format(major)  #updates according to the new major
-    # print(major)
-    if not names or major == 'History Major':
-        db_cursor.post(major_checker, 'do manually')
-        continue
-    for j in range(len(names)):
-        #splits the li element into the class number and the description
-        if major == 'Economics Major':
-            names[j] = names[j].split(',')[0]
-        else:
-            names[j] = names[j].split(':')[0]
-        db_cursor.post(major_checker, names[j])
-    time.sleep(5)
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+
+
+sheet = client.open("Majors - Requirements").sheet1
+
+majors = sheet.col_values(1)
+
+for major in range(1, (1 + len(majors))):
+    classes = sheet.row_values(major)
+    i = major - 1
+    path = '/Major_and_Reqs/{}'.format(majors[i])
+    for i in range(1, len(classes)):
+        db.post(path, classes[i])
--- scrapcode.py
+++ scrapcode.py
@@ -74,3 +74,89 @@ soup = BeautifulSoup(req.text, "html.parser")
 print(soup.prettify)
 
 print(soup.find_all('table'))
+
+
+
+
+
+
+
+
+<!-------plugin lists onto the excel sheet---------->
+
+# j = 0
+# for i in range(2, (2 + len(anthro_major))):
+#     sheet.update_cell(1, i, anthro_major[j])
+#     j = j + 1
+
+econ = ['ECON 401', 'ECON 402', 'STATS 250',
+        'STATS 280', 'STATS 426', 'ECON 451',
+        'ECON 453', 'ECON 251', 'ECON 452', 'ECON 454']
+
+comm = ['COMM ' + str(i) for i in range(200, 500)]
+
+his = ['HISTORY 202', 'HISTORY 497', 'HISTORY 496', 'HISTORY 499']
+
+lin = ['LING 313', 'LING 315', 'LING 316', 'LING 497']
+
+men = ['MENAS 493', 'HISTORY 443', 'POLSCI 351', 'POLSCI 352', 'POLSCI 353']
+
+list_of_lists = [anthro_major, econ, comm, his, lin, men]
+
+
+j = 0
+k = 1
+for major in list_of_lists:
+    j = 0
+    for i in range(2, (2 + len(major))):
+        sheet.update_cell(k, i, major[j])
+        j = j + 1
+    k = k + 1
+
+
+
+
+
+
+
+<!----------importing directly from the website to the database>
+
+
+import time
+from firebase import firebase
+from selenium import webdriver
+from just_scraping import *
+
+db_cursor = firebase.FirebaseApplication('https://web-scrapping-de6ca.firebaseio.com/', None)
+
+db_cursor.put('/', 'open_major_url', None)
+
+for i in range(len(name_of_majors)):
+    db_cursor.put('/open_major_url', name_of_majors[i].replace('[', '(').replace(']', ')').replace('.', '').replace('/', '_'), url_with_text[i])
+    if name_of_majors[i] == 'Biochemistry [B.S.] (Major)':
+        db_cursor.put('/open_major_url', 'Biochemistry (B.S.) (Major)', url_with_text[i])
+    else:
+        db_cursor.put('/open_major_url', name_of_majors[i], url_with_text[i])
+
+db_cursor.put('/', 'Major_and_Reqs', None)
+
+
+for i in range(len(url_tails)):
+    driver.get('https://lsa.umich.edu{}'.format(url_tails[i]))      # visits page of major
+    requirements_soup = BeautifulSoup(driver.page_source, "html.parser")  #creates new soup
+    target = requirements_soup.select('div.current-program-requirements div.reqs ul li')  #Only looks for the classes (ideally, this is broken)
+    names = [n.text for n in target]  #strips the text from the li tags
+    major = requirements_soup.h1.text.strip().replace('[', '(').replace(']', ')').replace('.', '').replace('/', '_')
+    major_checker = '/Major_and_Reqs/{}'.format(major)  #updates according to the new major
+    # print(major)
+    if not names or major == 'History Major':
+        db_cursor.post(major_checker, 'do manually')
+        continue
+    for j in range(len(names)):
+        #splits the li element into the class number and the description
+        if major == 'Economics Major':
+            names[j] = names[j].split(',')[0]
+        else:
+            names[j] = names[j].split(':')[0]
+        db_cursor.post(major_checker, names[j])
+    time.sleep(5)
